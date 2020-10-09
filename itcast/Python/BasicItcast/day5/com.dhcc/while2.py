276dbce0a946d82c87ec9bf8373ee09eb0c887f8
woainizaobu <woainizaobu@163.com>
2020-10-09 10:34:39 +0800

    add åadd to  pythen day5

---
+++ itcast/Python/BasicItcast/day5/com.dhcc/while2.py
@@ -0,0 +1,17 @@
+
+
+i = 0
+result = 0
+while i <= 100:
+    if i % 2 == 0:
+        result += i
+    i += 1
+print(result)
+
+
+j = 0
+result2 = 0
+while j <= 100:
+    result2 += j
+    j += 2
+print(result2)
