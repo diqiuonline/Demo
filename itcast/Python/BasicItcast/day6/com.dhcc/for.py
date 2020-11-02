33be7bcfba2a49e51573885a0d58aed5b142c876
diqiuonline <diqiuonline@gmail.com>
2020-11-02 23:18:37 +0800

    add python day6

---
+++ itcast/Python/BasicItcast/day6/com.dhcc/for.py
@@ -0,0 +1,12 @@
+"""
+for 零时变量 in 序列：
+    重复执行的代码
+    。。。。。
+"""
+str1 = 'itheima'
+for i in str1:
+    if i == 'e':
+        continue
+    print(i)
+
+
---
+++ itcast/Python/BasicItcast/day6/com.dhcc/whereelse.py
@@ -0,0 +1,9 @@
+i = 1
+while i <=5:
+    i += 1
+    if i == 4:
+        continue
+    print('我错了')
+
+else:
+    print('媳妇原谅我了')
