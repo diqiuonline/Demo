255d4056271f46aaf0688d304dab71228e60317f
diqiuonline <diqiuonline@gmail.com>
2022-07-16 10:34:46 +0800

    cache day 1

---
+++ study/longguo/esho-cache/src/main/java/com/roncoo/eshop/cache/configuration/CacheConfiguration.java
@@ -0,0 +1,31 @@
+package com.roncoo.eshop.cache.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @Author 李锦卓
+ * @Description 缓存配置管理类
+ * @Date 2022/7/9 18:01
+ * @Version 1.0
+ */
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        ehCacheManagerFactoryBean.setShared(true);
+        return ehCacheManagerFactoryBean;
+    }
+
+    @Bean
+    public EhCacheCacheManager ehCacheCacheManager(EhCacheManagerFactoryBean bean) {
+        return new EhCacheCacheManager(bean.getObject());
+    }
+}
